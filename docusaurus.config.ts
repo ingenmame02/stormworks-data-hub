528943ef35cf3cda3e124dde5b3b0dd7605ec987
Ingenmame <ingenmame02@gmail.com>
2026-06-28 11:01:11 +0900

    img change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,7 +54,7 @@ const config: Config = {
   ],
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/stormworks_data_hub_logo.png',
     colorMode: {
       respectPrefersColorScheme: true,
     },
